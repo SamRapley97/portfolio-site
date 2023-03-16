b8ad58fa8774058cf5a177423deb48181aa339ec
Sam <samrapley@protonmail.com>
2023-03-16 19:40:20 +0000

    added iteration to intro points

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 window.onload=function(){
 
+/*
+On page loading, wait until page is fully present. 
+Add an event listener that will check that this is correct.
+In a for loop, change bullet point CSS so that it is checked green box, hover in. 
+Only run once per session
+
+*/
+
+
+const introPoints = document.getElementById("intro_points").getElementsByTagName("li")
+console.log(introPoints)
+Array.from(introPoints).forEach((x, i) => {
+  setTimeout(function(){
+    console.log(x)
+    x.style.color = "blue"
+    console.log("${x} is working")
+  }, i * 1000)
+});
+  
+
+
+function greenCheckmark(x) {
+  
+}
+
+
+
+
+ 
+
 const menuOptions = document.getElementById("menu_options")
 menuOptions.addEventListener('click', (e) =>
   {
