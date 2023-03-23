9c0f343ddda44755b33eb23dae4935bdff2f35c2
Sam <samrapley@protonmail.com>
2023-03-23 08:38:30 +0000

    added delay to checklist, made sure other button click does not affect it

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 window.onload=function(){
 
 
+carousel = document.getElementById('carousel')
+carousel.scrollLeft = (carousel.scrollWidth - carousel.clientWidth) / 2
+
+
 
 // Delay loop for the intro text list elements
 const delayLoop = (fn, delay) => {
@@ -11,14 +15,27 @@ const delayLoop = (fn, delay) => {
     }
 };
 
+
+
 let cards = document.querySelectorAll('.card');
 cards = [...cards]
-
-let frontCards = document.getElementsByClassName('front');
-frontCards = [...frontCards]
-
 const introPoints = document.getElementById("intro_points").getElementsByTagName("li")
-// const introPoints = document.getElementById("intro_points").getElementsByTagName("li")
+const checklistCardButton = document.getElementById('right_card_button')
+
+checklistCardButton.addEventListener( 'click', function(){
+  this.disabled = true
+  if(cards[1].className == "card right_card" ){
+    const checkBox = x => x.classList.add('checkbox')
+    Array.from(introPoints).forEach(delayLoop(checkBox, 700)) 
+    setTimeout(() => {this.disabled=false}, 4500)
+  } 
+  else {
+    const removeCheckBox = x => x.classList.remove('checkbox')
+    Array.from(introPoints).forEach(removeCheckBox)
+    this.disabled = false;
+  }
+
+})
 
 cards.forEach((card)=>{
   
@@ -30,88 +47,35 @@ cards.forEach((card)=>{
       let backCards = [...card.getElementsByClassName('back')]
       frontCards[0].classList.add('front_hidden')
       backCards[0].classList.remove('back_hidden')
-      const checkBox = x => x.classList.add('checkbox')
-      Array.from(introPoints).forEach(delayLoop(checkBox, 700)) 
-      
-      
     }
     else {
       let frontCards = [...card.getElementsByClassName('front')]
       let backCards = [...card.getElementsByClassName('back')]
       frontCards[0].classList.remove('front_hidden')
       backCards[0].classList.add('back_hidden')
-      const removeCheckBox = x => x.classList.remove('checkbox')
-      Array.from(introPoints).forEach(removeCheckBox)
-
-    
-
-    }
-      // backCards[0].classList.add('back_hidden')
-
-    //   frontCards.forEach((x) => {
-    //     x.classList.remove('back_hidden')
-    //     x.classList.add('front_hidden')
-
-    //   })
-    // } 
-    // else if(card.classList.contains('is-flipped') == false) {
-    //   frontCards.forEach((x) => {
-    //     x.classList.remove('front_hidden')
-    //     x.classList.add('back_hidden')
-    //   })
       
-    
-    
+    }
+      
     })
-    
 
   });
 
 
+  const menuOptions = document.getElementById("menu_options")
 
-// Checks whether list animation has already run.
-// if (!('hasCodeRunBefore' in localStorage)){
+  // Adds an event for when one of the menu items is clicked on. 
+  menuOptions.addEventListener('click', (e) =>
+    {
+  // Assigns a variable equal to the clicked on menu option's name property. 
+      let elementName = e.target.name; 
+  // Sets a timeout so as to ensure that action can be initialised, runs smooth scroll action
+      setTimeout(function () {
+          document.getElementById(elementName).scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+              
+          });
+      }, 100);
+    }
+  )}
 
-// // Gets the individual list elements, converys into an array and adds class of "checkbox to each one"
-//   const introPoints = document.getElementById("intro_points").getElementsByTagName("li")
-//   const checkBox = x => x.className = "checkbox"
-//   Array.from(introPoints).forEach(delayLoop(checkBox, 1000)) 
-
-// // // Once run, sends to local storage so as to prevent running on every reload.
-//   localStorage.setItem("hasCodeRunBefore", true);  
-// } 
-// else {
-// // If local cache exists, gets all the elements and assigns them correct class
-//   const introPoints = document.getElementById("intro_points").getElementsByTagName("li")
-//   Array.from(introPoints).forEach((x) => {
-//   x.className = "checkbox"
-// });
-
-// }
-
-const menuOptions = document.getElementById("menu_options")
-
-// Adds an event for when one of the menu items is clicked on. 
-menuOptions.addEventListener('click', (e) =>
-  {
-// Assigns a variable equal to the clicked on menu option's name property. 
-    let elementName = e.target.name; 
-// Sets a timeout so as to ensure that action can be initialised, runs smooth scroll action
-    setTimeout(function () {
-        document.getElementById(elementName).scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-            
-        });
-    }, 100);
-  }
-)
-
-
-
-carousel = document.getElementById('carousel')
-
-carousel.scrollLeft = (carousel.scrollWidth - carousel.clientWidth) / 2
-
-
-}
