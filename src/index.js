117b4722a038fd25211c1130b722ae8715715830
Sam (work) <samwork@Sams-MBP.fritz.box>
2023-03-24 17:06:59 +0000

    local dev branch created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import "./styles.css"
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCfVAHmazdBEbedaMgRXTioNRz15r0f56w",
+  authDomain: "portfolio-site-staging-eb356.firebaseapp.com",
+  projectId: "portfolio-site-staging-eb356",
+  storageBucket: "portfolio-site-staging-eb356.appspot.com",
+  messagingSenderId: "989341024010",
+  appId: "1:989341024010:web:ef84e194c6aba63240c4f3",
+  measurementId: "G-DKX65NXSFY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 window.onload=function(){
 
 
