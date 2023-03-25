7ceb9f5fd3f19b8170d043ec3f586fa594d2a764
Sam <samrapley@protonmail.com>
2023-03-25 11:07:36 +0000

    deleted extra google config

--- src/index.js
+++ src/index.js
@@ -22,9 +22,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 window.onload=function(){
 
