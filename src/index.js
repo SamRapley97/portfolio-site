803cb9b56fd2a7ebc9ff3d862bda2d25fa9c16c8
Sam <samrapley@protonmail.com>
2023-03-25 11:08:11 +0000

    removed extra config

--- src/index.js
+++ src/index.js
@@ -22,9 +22,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 
 window.onload=function(){
 
